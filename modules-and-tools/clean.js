0bf3e02caf0e942be2cf3dd864443c2c5cc3e5c3
Kuba Sobecki <gosustyle@gmail.com>
2022-10-28 10:57:18 +0200

    Fix some bad code

--- modules-and-tools/clean.js
+++ modules-and-tools/clean.js
@@ -1,4 +1,15 @@
-var budget = [
+// var budget = [
+//   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+//   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+//   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+//   { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+//   { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+//   { value: -20, description: 'Candy 🍭', user: 'matilda' },
+//   { value: -125, description: 'Toys 🚂', user: 'matilda' },
+//   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+// ];
+
+const budget = [
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -9,56 +20,95 @@ var budget = [
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ];
 
-var limits = {
+// var limits = {
+//   jonas: 1500,
+//   matilda: 100,
+// };
+
+const spendingLimits = {
   jonas: 1500,
   matilda: 100,
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
+const getLimit = user => spendingLimits?.[user] ?? 0;
+
+// var add = function (value, description, user) {
+//   if (!user) user = 'jonas';
+//   user = user.toLowerCase();
+
+//   var lim;
+//   if (limits[user]) {
+//     lim = limits[user];
+//   } else {
+//     lim = 0;
+//   }
+
+//   if (value <= lim) {
+//     budget.push({ value: -value, description: description, user: user });
+//   }
+// };
+
+const addExpense = function (value, description, user = 'jonas') {
   user = user.toLowerCase();
-
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
-
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
+  if (value <= getLimit(user))
+    budget.push({ value: -value, description, user });
 };
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
+// add(10, 'Pizza 🍕');
+// add(100, 'Going to movies 🍿', 'Matilda');
+// add(200, 'Stuff', 'Jay');
 
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
-  }
-};
-check();
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
 
 console.log(budget);
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
+// var check = function () {
+//   for (var el of budget) {
+//     var lim;
+//     if (limits[el.user]) {
+//       lim = limits[el.user];
+//     } else {
+//       lim = 0;
+//     }
+
+//     if (el.value < -lim) {
+//       el.flag = 'limit';
+//     }
+//   }
+// };
+// check();
+
+const checkExpenses = () => {
+  budget.forEach(el => {
+    if (-el.value > getLimit(el.user)) el.flag = 'limit';
+  });
 };
+checkExpenses();
+
+console.log(budget);
+
+// var bigExpenses = function (limit) {
+//   var output = '';
+//   for (var el of budget) {
+//     if (el.value <= -limit) {
+//       output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
+//     }
+//   }
+//   output = output.slice(0, -2); // Remove last '/ '
+//   console.log(output);
+// };
+
+const logBigExpenses = bigLimit =>
+  budget
+    .reduce(
+      (acc, el) =>
+        -el.value > bigLimit ? (acc += el.description.slice(-2) + ' / ') : acc,
+      ''
+    )
+    .slice(0, -2);
+
+console.log(logBigExpenses(spendingLimits.jonas));
+console.log(logBigExpenses(spendingLimits.matilda));
+console.log(logBigExpenses(10));

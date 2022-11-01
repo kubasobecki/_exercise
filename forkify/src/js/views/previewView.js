1ced16105ef7488b17064a6dde55842fcd13e9a7
Kuba Sobecki <gosustyle@gmail.com>
2022-11-01 18:34:03 +0100

    Clean up unused imports and add user-created icon to preview element

--- forkify/src/js/views/previewView.js
+++ forkify/src/js/views/previewView.js
@@ -16,6 +16,11 @@ class ResultsView extends View {
             <div class="preview__data">
                 <h4 class="preview__title">${this._data.title}</h4>
                 <p class="preview__publisher">${this._data.publisher}</p>
+                <div class="preview__user-generated hidden">
+                    <svg>
+                        <use href="${icons}#icon-user"></use>
+                    </svg>
+                </div>
             </div>
             </a>
         </li>

8cf7a228655a7be4e957abe64d12a5a52e99fd04
Kuba Sobecki <gosustyle@gmail.com>
2022-11-01 18:28:14 +0100

    Refactor resultsView and bookmarksView duplicate code to previewView AND fix results view not retaining proper pagination page after rendering a recipe

--- forkify/src/js/views/resultsView.js
+++ forkify/src/js/views/resultsView.js
@@ -1,4 +1,5 @@
 import View from './View.js';
+import previewView from './previewView.js';
 import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
@@ -7,25 +8,8 @@ class ResultsView extends View {
   _message = '';
 
   _generateMarkup() {
-    const curId = window.location.hash.slice(1);
-
     return this._data.reduce(
-      (html, item) =>
-        (html += `
-        <li class="preview">
-            <a class="preview__link ${
-              item.id === curId ? 'preview__link--active' : ''
-            }" href="#${item.id}">
-            <figure class="preview__fig">
-                <img src="${item.image}" alt="${item.title}" />
-            </figure>
-            <div class="preview__data">
-                <h4 class="preview__title">${item.title}</h4>
-                <p class="preview__publisher">${item.publisher}</p>
-            </div>
-            </a>
-        </li>
-    `),
+      (html, item) => html + previewView.render(item, false),
       ''
     );
   }
